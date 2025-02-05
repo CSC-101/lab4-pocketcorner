e9936439bbda837ac91ccc6d3d7ed310fe01acfb
Seven <ssuwandi@calpoly.edu>
2025-02-04 23:29:58 -0800

    First Commit

--- lab4.py
+++ lab4.py
@@ -4,11 +4,30 @@ import data
 
 # Part 1
 
+def first_element(ins:list[list[int]]) -> list[int]:
+    element_list = []
+    for sub_list in ins:
+        if len(sub_list) > 0:
+            element_list.append(sub_list[0])
+    return element_list
+
+
 
 # Part 2
+def x_coordinates(points:list[data.Point]) -> list[float]:
+    x_coords = [points.x for points in points]
+    print(x_coords)
+    return x_coords
+
 
 
 # Part 3
+def are_in_positive_quadrant(quads:list[data.Point]) -> list[data.Point]:
+    pos_points = [coords for coords in quads if coords[0]> 0 and coords[1] > 0]
+    print(pos_points)
+    return pos_points
+
+are_in_positive_quadrant([data.Point(-6,4), data.Point(2,-2049643), data.Point(4,43)])
 
 
 # Part 4
--- lab4_tests.py
+++ lab4_tests.py
@@ -13,13 +13,25 @@ class TestCases(unittest.TestCase):
         expected = [1, 3]
         self.assertEqual(expected, result)
 
-
     def test_first_element_2(self):
         # write a second test here
-
+        input = [[], []]
+        result = lab4.first_element(input)
+        expected = []
+        self.assertEqual(expected, result)
 
     # Part 2
+    def test_x_coordinate_1(self):
+        input = [data.Point(5,8)]
+        result = lab4.x_coordinates(input)
+        expected = [5]
+        self.assertEqual(expected, result)
 
+    def test_x_coordinate_2(self):
+        input = [data.Point(6,4), data.Point(2,2049643), data.Point(4,43)]
+        result = lab4.x_coordinates(input)
+        expected = [6,2,4]
+        self.assertEqual(expected, result)
 
     # Part 3
 
